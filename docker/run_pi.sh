6f7fa292c3ecdcca5e56b9ad9c6fb5e864dcd70c
LEE JEONG WOOK <tarpha@gmail.com>
2019-09-22 11:45:47 +0900

    for RasberryPi

---
+++ docker/run_pi.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+while true
+do
+  cd /torrssen2 && git pull && cd /
+  
+  cp /torrssen2/docker/torrssen2-*.jar torrssen2.jar
+  
+  java -jar torrssen2.jar
+  #java $JAVA_OPTS -Xshareclasses -Xquickstart -jar torrssen2.jar
+done
